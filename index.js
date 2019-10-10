088fe449128f132d2489ff82c355769e0fa7aa49
Leonid Shifrin <leonids@wolfram.com>
2019-10-10 23:43:00 +0300

    Add command line args and source folder removal

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const path = require("path");
 const {copyFile} = require("./copy-file-async");
-const {traverse, ensureDir} = require("./dir-ops");
+const {traverse, ensureDir, deleteFolderRecursiveSync} = require("./dir-ops");
+let argv = require('yargs');
+const wrapper = require("./callwrapper");
 
-const filedir = __dirname + "/SampleFiles/Westerns";
-const fileDest = __dirname + "/SampleFiles/NewWesterns";
+const defaultSource = __dirname + "/SampleFiles/Westerns";
+const defaultDestination = __dirname + "/SampleFiles/NewFiles";
 
 
 const getFirstLetter = str => str.charAt(0).toUpperCase();
 
-function categorizeFiles(sourceDir, targetDir, cb) {
+function categorizeFiles(sourceDir, targetDir, options, cb) {
+
+    const {deleteOriginalFolder, verbose, defaultSource} = options;
 
     const getNewDirForFile = filename => path.join(
         targetDir, getFirstLetter(path.basename(filename))
@@ -16,27 +20,78 @@ function categorizeFiles(sourceDir, targetDir, cb) {
 
     const getNewFileName = (filename, dir) => path.join(dir, path.basename(filename));
 
+    const taskMonitor = wrapper.createTaskMonitor(
+        taskCount => {
+            if(verbose){
+                console.log(`Current task count: ${taskCount}`)
+            }
+        },
+        () => {
+            if(verbose){
+                console.log("All copying tasks finished");
+            }
+            if(deleteOriginalFolder && (defaultSource !== source)){
+                if(verbose){
+                    console.log(`Deleting original folder ${source}`);
+                }
+                deleteFolderRecursiveSync(sourceDir)
+            }
+        }
+    );
+
     traverse(sourceDir, (err, filename) => {
-        if (err){
+        if (err) {
             cb(err)
         } else {
-            ensureDir(getNewDirForFile(filename), (err, dirpath) => {
+            wrapper.wrap(ensureDir)(getNewDirForFile(filename), (err, dirpath) => {
                 if (err) {
                     cb(err);
                 } else {
-                    copyFile(filename, getNewFileName(filename, dirpath), err => {
+                    const newfile = getNewFileName(filename, dirpath);
+                    if(verbose){
+                        console.log(`Copying file ${filename} into ${newfile}`);
+                    }
+                    wrapper.wrap(copyFile)(filename, newfile, err => {
                         if (err) cb(err);
                     });
                 }
             });
         }
     });
+    setTimeout(taskMonitor, 0);
 }
 
+argv = argv
+    .option('source', {
+        alias: 's',
+        description: 'Source folder'
+        })
+    .option('destination',{
+        alias: 'd',
+        description: 'Destination folder'
+    })
+    .option('verbose', {
+        alias: 'v',
+        type: 'boolean',
+        description: 'Run with verbose logging'
+    }).option('delete-source', {
+        alias: 'ds',
+        type: 'boolean',
+        description: 'Delete the source folder'
+    }).argv;
+
+const source = argv.source || defaultSource;
+const destination = argv.destination || defaultDestination;
+const deleteOriginalFolder = argv['delete-source'];
+const verbose = argv.verbose;
+
+
 categorizeFiles(
-    filedir,
-    fileDest,
+    source,
+    destination,
+    {verbose, deleteOriginalFolder, defaultSource},
     err => {
-        console.warn(`Something went wrong. The error was: ${err}`)
+        console.warn(`Something went wrong. The error was: ${err}`);
+        throw err;
     }
 );
