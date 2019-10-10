a4a5a004544f229ef0e94e425bd6b3dbcebcb993
Leonid Shifrin <leonids@wolfram.com>
2019-10-10 18:55:18 +0300

    Feractoring, better error handling

---
+++ dir-ops.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+const path = require("path");
+
+function traverse(filepath, cb) {
+    let innerError = false;
+    let innercb = (error, arg) => {
+        if(error){
+            innerError = true;
+        }
+        cb(error, arg)
+    };
+    fs.stat(filepath, (err, stats) => {
+        if (err) {
+            cb(err);
+        } else if (stats.isDirectory()) {
+            fs.readdir(filepath, (err, files) => {
+                if (err) {
+                    cb(err);
+                } else {
+                    for(let filename of files){
+                        if(innerError){
+                            break;
+                        }
+                        traverse(path.join(filepath, filename), innercb)
+                    }
+                }
+            });
+        } else if (stats.isFile()) {
+            cb(null, filepath);
+        } else {
+            cb(new Error(`Can't handle the file at path ${filepath}`));
+        }
+    });
+}
+
+
+const ensureDir = (dirpath, cb) => {
+    fs.stat(dirpath, (err, stats) => {
+        if (err || !stats.isDirectory()) {
+            fs.mkdir(dirpath, {recursive: true}, err => {
+                if (err) {
+                    cb(err);
+                } else {
+                    cb(null, dirpath);
+                }
+            });
+        }
+    });
+};
+
+
+module.exports = {traverse, ensureDir};
--- index.js
+++ index.js
@@ -1,57 +1,42 @@
-const fs = require("fs");
 const path = require("path");
-const { copyFile } = require("./copy-file-async");
+const {copyFile} = require("./copy-file-async");
+const {traverse, ensureDir} = require("./dir-ops");
 
 const filedir = __dirname + "/SampleFiles/Westerns";
 const fileDest = __dirname + "/SampleFiles/NewWesterns";
 
-function traverse(filepath, cb) {
-    fs.stat(filepath, (err, stats) => {
-        if (err) throw err;
-        if (stats.isDirectory()) {
-            fs.readdir(filepath, (err, files) => {
+
+const getFirstLetter = str => str.charAt(0).toUpperCase();
+
+function categorizeFiles(sourceDir, targetDir, cb) {
+
+    const getNewDirForFile = filename => path.join(
+        targetDir, getFirstLetter(path.basename(filename))
+    );
+
+    const getNewFileName = (filename, dir) => path.join(dir, path.basename(filename));
+
+    traverse(sourceDir, (err, filename) => {
+        if (err){
+            cb(err)
+        } else {
+            ensureDir(getNewDirForFile(filename), (err, dirpath) => {
                 if (err) {
                     cb(err);
                 } else {
-                    files.forEach(filename =>
-                        traverse(path.join(filepath, filename), cb)
-                    );
+                    copyFile(filename, getNewFileName(filename, dirpath), err => {
+                        if (err) cb(err);
+                    });
                 }
             });
-        } else if (stats.isFile()) {
-            cb(null, filepath);
-        } else {
-            cb(new Error(`Can't handle the file at path ${filepath}`));
         }
     });
 }
 
-// traverse(filedir, console.log);
-
-const ensureDir = (dirpath, cb) => {
-    fs.stat(dirpath, (err, stats) => {
-        if (err || !stats.isDirectory()) {
-            fs.mkdir(dirpath, { recursive: true }, err => {
-                if (err) {
-                    cb(err);
-                } else {
-                    cb(null, dirpath);
-                }
-            });
-        }
-    });
-};
-
-traverse(filedir, (err, filename) => {
-    if (err) throw err;
-    const fstLetter = path
-        .basename(filename)
-        .charAt(0)
-        .toUpperCase();
-    ensureDir(path.join(fileDest, fstLetter), (err, dirpath) => {
-        if (err) throw err;
-        copyFile(filename, path.join(dirpath, path.basename(filename)), err => {
-            if (err) throw err;
-        });
-    });
-});
+categorizeFiles(
+    filedir,
+    fileDest,
+    err => {
+        console.warn(`Something went wrong. The error was: ${err}`)
+    }
+);
